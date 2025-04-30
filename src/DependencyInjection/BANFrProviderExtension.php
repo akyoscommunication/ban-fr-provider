5926db1a214301179107c5f8e4c868a9372092c5
Lilian Carneau <lilian.akyos@gmail.com>
2025-04-30 16:54:02 +0200

    implement PrependExtensionInterface in BANFrProviderExtension and add prepend method

--- src/DependencyInjection/BANFrProviderExtension.php
+++ src/DependencyInjection/BANFrProviderExtension.php
@@ -2,14 +2,41 @@
 
 namespace Akyos\BANFrProvider\DependencyInjection;
 
+use Akyos\BANFrProvider\Factory\GeoBANFrFactory;
+use Akyos\BANFrProvider\Factory\GeoPFFactory;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\AssetMapper\AssetMapperInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class BANFrProviderExtension extends Extension
+class BANFrProviderExtension extends Extension implements PrependExtensionInterface
 {
+    public function prepend(ContainerBuilder $container)
+    {
+        $bundles = $container->getParameter('kernel.bundles');
+
+//        if (isset($bundles['BazingaGeocoderBundle'])) {
+//            $container->prependExtensionConfig('bazinga_geocoder', [
+//                'providers' => [
+//                    'geopf' => [
+//                        'factory' => GeoPFFactory::class,
+//                        'aliases' => [
+//                            'geocoder.provider.geopf_geocoder'
+//                        ]
+//                    ],
+//                    'BANFr' => [
+//                        'factory' => GeoBANFrFactory::class,
+//                        'aliases' => [
+//                            'geocoder.provider.BANFr_geocoder'
+//                        ]
+//                    ],
+//                ],
+//            ]);
+//        }
+    }
+
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
